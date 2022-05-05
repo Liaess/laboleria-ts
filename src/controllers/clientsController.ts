267aba376f4967255ba6ab604c88e665702183f1
Liaess <yann.melo@gmail.com>
2022-05-05 13:09:06 -0300

    feat: add error when no orders from client

--- src/controllers/clientsController.ts
+++ src/controllers/clientsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import NotFound from "../errors/NotFound";
 import * as clientsService from "../services/clientsService";
 import * as ordersService from "../services/ordersService";
 
@@ -11,5 +12,6 @@ export async function createClient(req: Request, res: Response) {
 export async function getAllOrdersFromClient(req: Request, res: Response) {
   const id = parseInt(req.params.id);
   const orders = await ordersService.getAllOrdersFromClient(id);
+  if (orders.length === 0) throw new NotFound("Nenhum pedido foi encontrado");
   res.status(httpStatus.OK).send(orders);
 }
